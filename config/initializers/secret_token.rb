cd001cd917717cf009db7a0a4f7955885ac1448c
Badarudheen Kunnathodi <hari@futurenow.biz>
2011-05-26 19:39:04 +0530

    before any change

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acenet0001::Application.config.secret_token = 'f4cee73e74c28be03ec2af794b3923ee2296f62b42e01762067f09c90f68f7cec7fd7b9e026aa828b2adfa7a644fd72dcfba4735ee61c58c08a86d9e0229ce4c'
