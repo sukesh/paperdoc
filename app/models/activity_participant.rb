a31251f577bbde75abf356917fb8fa6098cc8c6c
Badarudheen Kunnathodi <hari@futurenow.biz>
2011-06-01 01:15:48 +0530

    data model relations done -BKT

--- app/models/activity_participant.rb
+++ app/models/activity_participant.rb
@@ -1,2 +1,6 @@
 class ActivityParticipant < ActiveRecord::Base
+  # Since, an 'activityparticipant' is a user & 
+  # an 'activityparticipant' belongs to an 'activity'.
+  belongs_to :user
+  belongs_to :activity
 end
